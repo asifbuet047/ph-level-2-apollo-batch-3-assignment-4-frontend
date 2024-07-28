cc7534735a0e9b293534e82748ae56e48a24ee5f
asifbuet047 <asifbuet047@gmail.com>
2024-07-28 16:03:21 +0600

    In Mnage product page user can update any products info which is selected by a dropdown box and its previous data is loaded into form

--- src/pages/UpdateProductPage.tsx
+++ src/pages/UpdateProductPage.tsx
@@ -10,11 +10,12 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { TProduct } from "../types/AllTypes";
 import SingleProductCard from "../components/SingleProductCard";
+import { useResize } from "../utils/customHooks";
 
 function UpdateProductPage() {
   const products = useAppSelector((state) => state.products.products);
-  console.log(products);
   const [selectedProduct, setSelectedProduct] = useState<TProduct>(products[0]);
+  const [width, setWidth] = useState(0);
   const [updateProduct, { data, isError, isSuccess, isLoading }] =
     useUpdateproductMutation();
   const {
@@ -25,19 +26,20 @@ function UpdateProductPage() {
     formState: { errors },
   } = useForm();
   const refForWidth = useRef(null);
-  const [viewWidth, setViewWidth] = useState(0);
-  const [name, setName] = useState(selectedProduct.name);
 
   const submit = () => {
     console.log(getValues());
   };
 
   useEffect(() => {
-    setViewWidth(refForWidth.current.offsetWidth);
-  }, []);
+    setWidth(refForWidth.current.offsetWidth);
+  }, [refForWidth]);
 
   return (
-    <div className="flex flex-col items-center justify-between align-middle w-full border-2">
+    <div
+      ref={refForWidth}
+      className="flex flex-col items-center justify-between align-middle w-full border-2"
+    >
       <Autocomplete
         id="search-products"
         className="w-full mt-2 mb-2"
@@ -46,8 +48,10 @@ function UpdateProductPage() {
           <TextField {...params} label="Search Product" />
         )}
         onChange={(event, product) => {
-          setSelectedProduct(product);
-          console.log(selectedProduct);
+          setSelectedProduct(product as TProduct);
+          Object.keys(product as TProduct).forEach((element) => {
+            setValue(element, product[element]);
+          });
         }}
         getOptionLabel={(option) => option.name}
         getOptionKey={(option) => option._id}
@@ -65,126 +69,71 @@ function UpdateProductPage() {
       <div className="w-full flex flex-col items-center">
         <Card title="Update Product" className="rounded-lg border-2 w-full">
           <div>
-            {!isSuccess && (
+            {selectedProduct && (
               <form
                 onSubmit={handleSubmit(submit)}
                 className="flex flex-col justify-center align-middle items-center p-5 w-full"
                 encType="multipart/form-data"
                 ref={refForWidth}
               >
-                {selectedProduct ? (
-                  <input
-                    type="text"
-                    defaultValue={selectedProduct.name}
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("name", { required: true })}
-                  />
-                ) : (
-                  <input
-                    disabled
-                    type="text"
-                    placeholder="Product name"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("name", { required: true })}
-                  />
-                )}
-
+                <input
+                  type="text"
+                  defaultValue={selectedProduct.name}
+                  className="input input-bordered w-full max-w-xs mt-2 mb-2"
+                  {...register("name", { required: true })}
+                />
                 {errors.name && <p>Name is required</p>}
-                {selectedProduct ? (
-                  <input
-                    defaultValue={selectedProduct.description}
-                    type="text"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("description", {
-                      required: true,
-                    })}
-                  />
-                ) : (
-                  <input
-                    disabled
-                    type="text"
-                    placeholder="Product description"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("description", {
-                      required: true,
-                    })}
-                  />
-                )}
+
+                <input
+                  defaultValue={selectedProduct.description}
+                  type="text"
+                  className="input input-bordered w-full max-w-xs mt-2 mb-2"
+                  {...register("description", {
+                    required: true,
+                  })}
+                />
+
                 {errors.description && <p>Description is required</p>}
-                {selectedProduct ? (
-                  <input
-                    defaultValue={selectedProduct.category}
-                    type="text"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("category", { required: true })}
-                  />
-                ) : (
-                  <input
-                    disabled
-                    type="text"
-                    placeholder="Product category"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("category", { required: true })}
-                  />
-                )}
+
+                <input
+                  defaultValue={selectedProduct.category}
+                  type="text"
+                  className="input input-bordered w-full max-w-xs mt-2 mb-2"
+                  {...register("category", { required: true })}
+                />
+
                 {errors.category && <p>Category is required</p>}
-                {selectedProduct ? (
-                  <input
-                    type="text"
-                    defaultValue={selectedProduct.brand}
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("brand", { required: true })}
-                  />
-                ) : (
-                  <input
-                    disabled
-                    type="text"
-                    placeholder="Product brand name"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("brand", { required: true })}
-                  />
-                )}
+
+                <input
+                  type="text"
+                  defaultValue={selectedProduct.brand}
+                  className="input input-bordered w-full max-w-xs mt-2 mb-2"
+                  {...register("brand", { required: true })}
+                />
+
                 {errors.brand && <p>Brand is required</p>}
-                {selectedProduct ? (
-                  <input
-                    defaultValue={selectedProduct.quantity.toString()}
-                    type="number"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("quantity", { required: true })}
-                  />
-                ) : (
-                  <input
-                    disabled
-                    type="number"
-                    placeholder="Product quantity"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("quantity", { required: true })}
-                  />
-                )}
+
+                <input
+                  defaultValue={selectedProduct.quantity.toString()}
+                  type="number"
+                  className="input input-bordered w-full max-w-xs mt-2 mb-2"
+                  {...register("quantity", { required: true })}
+                />
 
                 {errors.quantity && <p>Quantity is required</p>}
-                {selectedProduct ? (
-                  <input
-                    type="number"
-                    defaultValue={selectedProduct.price.toString()}
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("price", { required: true })}
-                  />
-                ) : (
-                  <input
-                    disabled
-                    type="number"
-                    placeholder="Product unit price"
-                    className="input input-bordered w-full max-w-xs mt-2 mb-2"
-                    {...register("price", { required: true })}
-                  />
-                )}
+
+                <input
+                  type="number"
+                  defaultValue={selectedProduct.price.toString()}
+                  className="input input-bordered w-full max-w-xs mt-2 mb-2"
+                  {...register("price", { required: true })}
+                />
 
                 {errors.price && <p>Price is required</p>}
 
                 {isLoading && (
                   <p className="mt-2 mb-2">
-                    <BarLoader color="#36d7b7" width={viewWidth}></BarLoader>
+                    <BarLoader color="#36d7b7" width={width}></BarLoader>
                   </p>
                 )}
                 {isError && (
