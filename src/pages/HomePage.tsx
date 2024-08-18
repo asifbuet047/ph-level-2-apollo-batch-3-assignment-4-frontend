0a630a249de6d2ee484dad4de9f202ceb1466864
asifbuet047 <asifbuet047@gmail.com>
2024-08-18 10:19:53 +0600

    Discount banner work

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,7 +11,7 @@ function HomePage() {
   return (
     <div>
       {isSuccess && (
-        <Carousel autoplay={true}>
+        <Carousel autoplay={true} autoplaySpeed={5000} arrows={true}>
           {discounts.map((discount) => {
             return <DiscountComponent discount={discount}></DiscountComponent>;
           })}
