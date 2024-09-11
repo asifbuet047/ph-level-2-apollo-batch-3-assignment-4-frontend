802d87068bc6b7776f3a3d95771072895a252828
asifbuet047 <asifbuet047@gmail.com>
2024-09-12 01:19:06 +0600

    After successful payment cart is cleared

--- src/pages/PlaceOrderSuccessPage.tsx
+++ src/pages/PlaceOrderSuccessPage.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import Lottie from "react-lottie";
 import successful from "../../public/successful.json";
+import { useAppDispatch } from "../redux/hooks";
+import { clearCart } from "../redux/features/cartSlice";
 
 function PlaceOrderSuccessPage() {
+  const dispatch = useAppDispatch();
+  dispatch(clearCart());
   return (
     <div>
       <Lottie options={{ animationData: successful }}></Lottie>
