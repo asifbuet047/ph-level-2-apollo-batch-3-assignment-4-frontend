1818f7ccbedad506341269d38e95754299850715
asifbuet047 <asifbuet047@gmail.com>
2024-09-20 17:58:35 +0600

    Navigationbar is responsive now for mobile, laptopn and desktop screens

--- src/components/NavigationBarComponent.tsx
+++ src/components/NavigationBarComponent.tsx
@@ -7,14 +7,14 @@ import { TCartData } from "../types/AllTypes";
 function NavigationBarComponent() {
   const cart = useAppSelector((state) => state.cart.items) as TCartData[];
   return (
-    <div className="navbar bg-[#87F1FF] rounded-md justify-between">
-      <div className="rounded-full overflow-clip">
+    <div className="flex bg-[#87F1FF] flex-col md:flex-row rounded-md justify-between items-center px-2 py-2">
+      <div className="rounded-full overflow-clip xs:w-16 md:w-24 border-2 border-red-500">
         <a href="/">
-          <Image src="./logo_03.jpg" width={80} preview={false} />
+          <Image src="./logo_03.jpg" preview={false} />
         </a>
       </div>
-      <div className="">
-        <ul className="menu menu-horizontal px-1">
+      <div className="xs:w-full md:w-auto">
+        <ul className="menu menu-vertical md:menu-horizontal">
           <li className="pl-2 pr-2">
             <NavLink to={"/"}>Homepage</NavLink>
           </li>
@@ -33,12 +33,7 @@ function NavigationBarComponent() {
                 <NavLink to={"/cart"}>My Cart</NavLink>
               </Badge>
             ) : (
-              <Badge
-                variant="standard"
-                badgeContent={<Button shape="circle">0</Button>}
-              >
-                <NavLink to={"/cart"}>My Cart</NavLink>
-              </Badge>
+              <NavLink to={"/cart"}>My Cart</NavLink>
             )}
           </li>
           <li className="pl-2 pr-2">
